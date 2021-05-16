a598b46bc888863fd41fbd6ab9baead170c432b7
Choi-UngGyu <cdr982116@gmail.com>
2021-05-16 15:02:11 +0900

    폴더 이동

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
@@ -24,25 +24,39 @@ export default function Layout({ children, home }) {
             <header className={styles.header}>
                 {home ? (
                     <>
-                        <Image
+                        <img
                             priority
                             src="/images/profile.jpg"
                             className={utilStyles.borderCircle}
                             height={114}
                             width={114}
                             alt={name} />
+                        {/* <Image
+                            priority
+                            src="/images/profile.jpg"
+                            className={utilStyles.borderCircle}
+                            height={114}
+                            width={114}
+                            alt={name} /> */}
                     </>
                 ) : (
                     <>
                         <Link href="/">
                             <a>
-                                <Image
+                                <img
                                     priority
                                     src="/images/profile.jpg"
                                     className={utilStyles.borderCircle}
                                     height={108}
                                     width={108}
                                     alt={name} />
+                                {/* <Image
+                                    priority
+                                    src="/images/profile.jpg"
+                                    className={utilStyles.borderCircle}
+                                    height={108}
+                                    width={108}
+                                    alt={name} /> */}
                             </a>
                         </Link>
                         <h2 className={utilStyles.headingLg}>
