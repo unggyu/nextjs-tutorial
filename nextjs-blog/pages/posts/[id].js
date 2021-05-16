441ba58fbd337ea6a757e4994454eb339767d824
Choi-UngGyu <cdr982116@gmail.com>
2021-05-16 11:17:40 +0900

    getStaticProps 구현 02

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,14 +1,31 @@
 import Layout from '../../components/layout'
-import { getAllPostsIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticPaths() {
-    const paths = getAllPostsIds();
+    const paths = getAllPostIds();
     return {
         paths,
         fallback: false
     }
 }
 
-export default function Post() {
-    return <Layout>...</Layout>
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+        </Layout>
+    )
 }
